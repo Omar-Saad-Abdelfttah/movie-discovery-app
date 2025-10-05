875869b88150d45b6c8e29f7c2df4bc4f7c3fbdc
omar saad farag <168087590+Omarsaadfrg@users.noreply.github.com>
2025-10-05 13:16:31 +0300

    Add MovieGridScreen UI and movie data setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation("androidx.compose.foundation:foundation")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
