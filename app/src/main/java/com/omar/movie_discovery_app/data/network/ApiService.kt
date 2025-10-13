e011d569e09578cd87595edae2e80f9e0f9186ab
ahmed-magdy-gitt <ahmedmagdy707007@gmail.com>
2025-10-13 17:56:30 +0300

    Create ApiService interface file

--- app/src/main/java/com/omar/movie_discovery_app/data/network/ApiService.kt
+++ app/src/main/java/com/omar/movie_discovery_app/data/network/ApiService.kt
@@ -1,4 +1,13 @@
 package com.omar.movie_discovery_app.data.network
 
-class ApiService {
+import com.omar.movie_discovery_app.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String
+    ): MovieResponse
 }
