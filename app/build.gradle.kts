e7768fb5a6046be07792c33e2f33c3d4331a6814
omar saad farag <168087590+Omarsaadfrg@users.noreply.github.com>
2025-10-13 19:52:48 +0300

    add Retrofit, Gson converter, and OkHttp logging dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,12 @@ dependencies {
 
     implementation("androidx.compose.foundation:foundation")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,6 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-
 }
+
