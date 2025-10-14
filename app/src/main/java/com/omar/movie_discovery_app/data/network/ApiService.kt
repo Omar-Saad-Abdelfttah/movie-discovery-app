9685231845f11dee07a089835983416ad5b4aadd
omar saad farag <168087590+Omarsaadfrg@users.noreply.github.com>
2025-10-14 15:18:18 +0300

    Update ApiService to use ApiMovieResponse

--- app/src/main/java/com/omar/movie_discovery_app/data/network/ApiService.kt
+++ app/src/main/java/com/omar/movie_discovery_app/data/network/ApiService.kt
@@ -1,14 +1,13 @@
 package com.omar.movie_discovery_app.data.network
 
-import com.omar.movie_discovery_app.model.MovieResponse
+import com.omar.movie_discovery_app.model.API.Model.ApiMovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
-
 
 interface ApiService {
 
     @GET("movie/popular")
     suspend fun getPopularMovies(
         @Query("api_key") apiKey: String
-    ): MovieResponse
+    ): ApiMovieResponse
 }
