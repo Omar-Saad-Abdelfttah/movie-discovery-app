7e7ae32137abe7de436ff9b4b0c081577075bcd9
omar saad farag <168087590+Omarsaadfrg@users.noreply.github.com>
2025-10-16 14:06:27 +0300

    ubdate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
 }
