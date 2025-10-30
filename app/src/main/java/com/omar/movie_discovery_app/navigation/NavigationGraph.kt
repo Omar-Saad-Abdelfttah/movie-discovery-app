5c5c573c2febaa573eb917b8f5eb130f6dcbf5fb
Bassem <bassem.r.ibraheem@gmail.com>
2025-10-30 17:32:14 +0300

    add SplashScreen route and set startDestination to splash

---
+++ app/src/main/java/com/omar/movie_discovery_app/navigation/NavigationGraph.kt
@@ -0,0 +1,14 @@
+package com.omar.movie_discovery_app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.omar.movie_discovery_app.ui.details.MovieDetailsScreenById
+import com.omar.movie_discovery_app.ui.screens.MovieGridScreen
+import com.omar.movie_discovery_app.ui.screens.SearchScreen
+import com.omar.movie_discovery_app.ui.screens.SplashScreen
+
+
