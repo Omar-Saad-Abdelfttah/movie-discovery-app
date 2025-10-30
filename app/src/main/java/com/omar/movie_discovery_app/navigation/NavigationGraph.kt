678af5e292ad9540c0044d8556159473286b853f
Bassem <bassem.r.ibraheem@gmail.com>
2025-10-30 17:33:21 +0300

    implement NavigationGraph with Splash, Home, Search, and Details routes

--- app/src/main/java/com/omar/movie_discovery_app/navigation/NavigationGraph.kt
+++ app/src/main/java/com/omar/movie_discovery_app/navigation/NavigationGraph.kt
@@ -11,4 +11,47 @@ import com.omar.movie_discovery_app.ui.screens.MovieGridScreen
 import com.omar.movie_discovery_app.ui.screens.SearchScreen
 import com.omar.movie_discovery_app.ui.screens.SplashScreen
 
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
 
+
+        composable("splash") {
+            SplashScreen(navController = navController)
+        }
+
+        composable("home") {
+            MovieGridScreen(
+                onMovieClick = { movie ->
+                    navController.navigate("details/${movie.id}")
+                },
+                onSearchClick = {
+                    navController.navigate("search")
+                }
+            )
+        }
+
+        composable("search") {
+            SearchScreen(
+                navController = navController,
+                onMovieClick = { movie ->
+                    navController.navigate("details/${movie.id}")
+                }
+            )
+        }
+
+        composable(
+            route = "details/{movieId}",
+            arguments = listOf(navArgument("movieId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val movieId = backStackEntry.arguments?.getInt("movieId") ?: return@composable
+            MovieDetailsScreenById(
+                navController = navController,
+                movieId = movieId
+            )
+        }
+    }
+}
