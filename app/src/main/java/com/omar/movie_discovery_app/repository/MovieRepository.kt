5f91557a1f95f7853fcf77bbac2cc706a931d495
Bassem <bassem.r.ibraheem@gmail.com>
2025-10-30 18:05:57 +0300

    update

--- app/src/main/java/com/omar/movie_discovery_app/repository/MovieRepository.kt
+++ app/src/main/java/com/omar/movie_discovery_app/repository/MovieRepository.kt
@@ -17,13 +17,19 @@ class MovieRepository {
 
         return apiMovies.map { it.toMovie() }
     }
+
+
     suspend fun fetchMovieDetails(movieId: Int): MovieDetailsResponse {
         return RetrofitInstance.api.getMovieDetails(movieId, apiKey)
     }
+
+
 
     suspend fun searchMovies(query: String): List<Movie> {
         val response = RetrofitInstance.api.searchMovies(apiKey, query)
         return response.results.map { it.toMovie() }
     }
+
+
 
 }
