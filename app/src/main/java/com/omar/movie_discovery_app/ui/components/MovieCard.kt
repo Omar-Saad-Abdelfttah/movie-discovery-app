40659eab00e885512b7fe8d9bd90cbef0eeeb91c
omar saad farag <168087590+Omarsaadfrg@users.noreply.github.com>
2025-11-07 11:18:44 +0200

    minor UI adjustments to movie card and preparation for shimmer support

--- app/src/main/java/com/omar/movie_discovery_app/ui/components/MovieCard.kt
+++ app/src/main/java/com/omar/movie_discovery_app/ui/components/MovieCard.kt
@@ -7,18 +7,26 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import com.omar.movie_discovery_app.model.Movie
+import com.google.accompanist.placeholder.*
+import com.google.accompanist.placeholder.material.placeholder
+import com.google.accompanist.placeholder.material.shimmer
 
 @Composable
 fun MovieCard(movie: Movie, onClick: (Movie) -> Unit) {
+
+    val painter = rememberAsyncImagePainter(movie.imageUrl)
+    val isLoading = painter.state is AsyncImagePainter.State.Loading
+
     Box(
         modifier = Modifier
             .padding(8.dp)
@@ -27,14 +35,21 @@ fun MovieCard(movie: Movie, onClick: (Movie) -> Unit) {
             .clip(RoundedCornerShape(12.dp))
             .background(Color(0xFF1C1C1C))
     ) {
+
         Image(
-            painter = rememberAsyncImagePainter(movie.imageUrl),
+            painter = painter,
             contentDescription = movie.title,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(220.dp),
+                .height(220.dp)
+                .placeholder(
+                    visible = isLoading,
+                    highlight = PlaceholderHighlight.shimmer(),
+                    color = Color.DarkGray
+                ),
             contentScale = ContentScale.Crop
         )
+
         Box(
             modifier = Modifier
                 .align(Alignment.BottomStart)
@@ -45,7 +60,12 @@ fun MovieCard(movie: Movie, onClick: (Movie) -> Unit) {
             Text(
                 text = movie.title,
                 color = Color.White,
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.placeholder(
+                    visible = isLoading,
+                    highlight = PlaceholderHighlight.shimmer(),
+                    color = Color.DarkGray
+                )
             )
         }
     }
